c2bb65225f69d2c3478cce8c6797990689d82e0f
Pramod Poshettiwar <pramodposhettiwar@gmail.com>
2026-06-08 11:57:37 +0530

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -5,16 +5,16 @@ variable "sub_cidr" {
     default = "10.0.1.0/24"
 }
 variable "AWS_REGION" {
-    default = "ap-southeast-1"
+    default = "ap-south-2"
 }
 variable "amazon_AMI" {
     default = "ami-0543dbdaf4e114be7"
 }
 variable "amazon_instance_type" {
-    default = "t2.micro"
+    default = "t3.micro"
 }
 variable "kp" {
-    default = "singapore-linux-keypair-2026"
+    default = "linux-hyderabad-keypair-2026"
 }
 variable "akey" {
     default = ""
